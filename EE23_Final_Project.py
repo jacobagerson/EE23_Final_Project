cb3819f575985a05bd58e04400bc5d13f371c1f6
Jacob <jacob@MacBook-Pro-680.local>
2026-04-15 15:34:21 -0400

    Jacob

---
+++ EE23_Final_Project.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+import mido
+import soundfile 
+
+#Extract audio file, using librosa to turn it into a numpy array
+
+
+#Let's view the audio file as a waveform both in time and frequency domain, using librosa's display module
+
+
+#Let's calculate the Nyquist frequency and then sample the audio file at that frequency,
